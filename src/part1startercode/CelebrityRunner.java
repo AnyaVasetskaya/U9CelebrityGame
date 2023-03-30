caffc6b168749ddd91c11b4b90a134f8eb4294dc
AnyaVasetskaya <annav27@nycstudents.net>
2023-03-30 12:04:39 -0400

    initial

---
+++ src/part1startercode/CelebrityRunner.java
@@ -0,0 +1,16 @@
+package part1startercode;
+
+/**
+ * Starts the Celebrity Game application
+ * @author cody.henrichsen
+ *
+ */
+public class CelebrityRunner {
+	/**
+	 * The entry point of the Java application.
+	 * @param args Unused parameter as it is a GUI based program.
+	 */
+	public static void main(String [] args) {
+		CelebrityGame game = new CelebrityGame();
+	}
+}
